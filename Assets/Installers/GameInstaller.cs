b49bca29216eec19a02ba0411f5339e7e68f5f34
Ogunbayik <ogun.bayikk@gmail.com>
2026-01-05 21:38:10 +0300

    Basic BindSignalType created

--- Assets/Installers/GameInstaller.cs
+++ Assets/Installers/GameInstaller.cs
@@ -6,8 +6,13 @@ public class GameInstaller : MonoInstaller
     public override void InstallBindings()
     {
         SignalBusInstaller.Install(Container);
-
         Container.DeclareSignal<GameSignal.PlayerHitSignal>();
+
+        Container.Bind<AudioManager>().FromComponentInHierarchy().AsSingle();
+
+        Container.BindSignal<GameSignal.PlayerHitSignal>()
+            .ToMethod<AudioManager>(x => x.PlayHitSound)
+            .FromResolve();
 
     }
 }
